9c97af685ab6e1063bc69ab9364f94fa47a17f3d
XCodeBlocks <XCodeBlocks@gmail.com>
2017-03-20 17:42:04 +0900

    checkingAccount - 01차

--- OOP Lab 3/src/checkingAccount.java
+++ OOP Lab 3/src/checkingAccount.java
@@ -1,13 +1,18 @@
 
 public class checkingAccount extends Account {
-
-	checkingAccount() {
-		// TODO Auto-generated constructor stub
-	}
-
+	
+	//[변수선언]
+	private int		credit_limit	;
+	private double	interest		;
+	private double	loan_interest	;
+	
+	//[생성자]
 	checkingAccount(double input) {
 		super(input);
-		// TODO Auto-generated constructor stub
+		credit_limit	= 50	;
+		interest		= 0.01	;
+		loan_interest	= 0.07	;
 	}
-
+	
+	
 }
