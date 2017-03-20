0057de5feb10a27a60036b22dbec6736c6483236
XCodeBlocks <XCodeBlocks@gmail.com>
2017-03-20 17:02:48 +0900

    (소스코드 추가만)

---
+++ OOP Lab 3/src/checkingAccount.java
@@ -0,0 +1,13 @@
+
+public class checkingAccount extends Account {
+
+	public checkingAccount() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public checkingAccount(double money) {
+		super(money);
+		// TODO Auto-generated constructor stub
+	}
+
+}
