33f02a8a868fa641b91857a66df5f9b520f18a71
XCodeBlocks <XCodeBlocks@gmail.com>
2017-03-27 17:04:53 +0900

    AccountTest.java 수정 - 2

--- OOP Lab 3/src/AccountTest.java
+++ OOP Lab 3/src/AccountTest.java
@@ -7,7 +7,7 @@ public class AccountTest {
 	
 	public static void main(String[] args) {
 		
-		double input;
+		double amount;
 //[1문단]		
 		//(2개 account 초기화)
 		CheckingAccount account1 = new CheckingAccount(100.00);
@@ -20,8 +20,8 @@ public class AccountTest {
 		
 		System.out.print("Enter deposit amount for ccount1: ");
 		Scanner scan = new Scanner(System.in);
-		input = scan.nextDouble();
-		account1.credit(input);				//(900)
+		amount = scan.nextDouble();
+		account1.credit(amount);				//(900)
 
 //[2문단]
 		System.out.print("account1 balance: ");
@@ -33,8 +33,8 @@ public class AccountTest {
 		
 		System.out.print("Enter withdrawl amount for account2: ");
 		//	sc = new Scanner(System.in);		//(이미 sc 객체를 만들었으니 선언 생략)
-		input = scan.nextDouble();
-		account2.debit(input);				//(10.00)
+		amount = scan.nextDouble();
+		account2.debit(amount);				//(10.00)
 		
 //[3문단]
 		System.out.print("account1 balance: ");
